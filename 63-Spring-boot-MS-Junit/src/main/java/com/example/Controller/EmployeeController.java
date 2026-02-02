c6f4f7a68f669df9da6a157fec88e42a0f57705e
vivekanandagiri <vivekanandagiri005@gmail.com>
2026-02-02 14:51:11 +0530

    "id" added to delete mapping

--- 63-Spring-boot-MS-Junit/src/main/java/com/example/Controller/EmployeeController.java
+++ 63-Spring-boot-MS-Junit/src/main/java/com/example/Controller/EmployeeController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,7 +44,7 @@ public class EmployeeController {
 	public EmployeeDTO updateEmployee(@PathVariable Long id, @RequestBody EmployeeDTO dto) throws EmployeeNotFoundException {
 		return employeeService.updateEmployee(id, dto);
 	}
-	
+	@DeleteMapping("/{id}")
 	public void deleteEmp(@PathVariable Long id) throws EmployeeNotFoundException {
 		employeeService.deleteEmployee(id);
 	}
